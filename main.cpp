b80219dda44130d97206252bc3fbae2f86200b18
Babakbvv <amirbabak.babaei@gmail.com>
2025-06-26 16:17:47 +0330

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    cout<<"hello word;
+    if(1){
+        cout<<"hello";
+        }
     MainWindow w;
     w.show();
     return a.exec();
